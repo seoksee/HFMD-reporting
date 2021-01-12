c847d086e866d1f75e075ad3be3d6d18373a175e
Seok See <seoksee@outlook.com>
2021-01-12 17:11:01 +0800

    forceSchemehttps

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illluminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,5 +25,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        $this->app['request']->server->set('HTTPS', true);
+        URL::forceScheme('https');
     }
 }
