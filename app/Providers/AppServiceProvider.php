89096fe1388a4ade0e10771aeb0498fa9c7469c0
Seok See <seoksee@outlook.com>
2021-01-12 17:12:29 +0800

    forceSchemehttps

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illluminate\Support\Facades\URL;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
